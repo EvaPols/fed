2975ddfcded8c6d182d3162776457ed6bee2d5c9
EvaPols <eva.pols@hva.nl>
2025-10-02 11:27:10 +0200

    Add files via upload

---
+++ scripts/script.js
@@ -0,0 +1,14 @@
+let openMenuButton = document.querySelector("nav:nth-of-type(2) button");
+let sluitMenuButton = document.querySelector("nav:nth-of-type(3) button");
+let menuNav = document.querySelector("nav:nth-of-type(3)");
+
+function openMenu() {
+    menuNav.classList.add("is-open");
+}
+
+function sluitMenu() {
+    menuNav.classList.remove("is-open");
+}
+
+openMenuButton.addEventListener("click", openMenu);
+sluitMenuButton.addEventListener("click", sluitMenu);
